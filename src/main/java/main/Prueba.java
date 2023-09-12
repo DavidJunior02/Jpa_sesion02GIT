43f74598695052a5f0d12e9ca1c4dcbee58433f0
El Profesor <i202023136@cibertec.edu.pe>
2023-09-11 21:53:45 -0500

    listar usuarios mediante procedimientos almacenados

--- src/main/java/main/Prueba.java
+++ src/main/java/main/Prueba.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbUsuario;
 
@@ -15,10 +16,15 @@ public class Prueba {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_Clase2x");
 		// 2. crea el manejador de entidades
 		EntityManager em = fabrica.createEntityManager();
+		
+		
+		Query query=em.createNativeQuery("{call findall_usuarios()}",TbUsuario.class);
+		List<TbUsuario> tabusu=query.getResultList();
+				
+		
 		System.out.println("===JAVA LIST====");
-		List<TbUsuario> listado = em.createQuery("Select a from TbUsuario a", TbUsuario.class).getResultList();
 		System.out.println("=====================================");
-		System.out.println("Numero de Usuarios: " + listado.size());
+		System.out.println("Numero de Usuarios: " + tabusu.size());
 		System.out.println("=====================================");
 	}
 }
